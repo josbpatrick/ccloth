b984e0d96265c7fc55f2a3dcd7c4056f4d568433
josbpatrick <josbpatrick@gmail.com>
2021-02-06 02:30:15 -0500

    updated mobile responsive css

--- client/src/components/collection-item/collection-item.styles.jsx
+++ client/src/components/collection-item/collection-item.styles.jsx
@@ -19,7 +19,21 @@ export const CollectionItemContainer = styled.div`
       display: flex;
     }
   }
-`;
+
+  @media screen and (max-width: 800px) {
+    width: 40vw;
+  
+    &:hover {
+      .image {
+        opacity: unset;
+      }
+  
+      button {
+        opacity: unset;
+      }
+    }
+  }
+  `;
 
 export const AddButton = styled(CustomButton)`
   width: 80%;
@@ -27,6 +41,13 @@ export const AddButton = styled(CustomButton)`
   position: absolute;
   top: 255px;
   display: none;
+
+  @media screen and (max-width: 800px) {
+    display: block;
+    opacity: 0.9%;
+    min-width: unset;
+    padding: 0px 10px;
+  }
 `;
 
 export const BackgroundImage = styled.div`
